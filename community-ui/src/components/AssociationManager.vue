d6119702cd9917a87777b9bbec3f3e7b056e6711
chenzhixiang <y51288033@outlook.com>
2023-10-23 17:45:50 +0800

    管理面板的搭建(活动、用户、班级、社团）

---
+++ community-ui/src/components/AssociationManager.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+123123
+
+    </div>
+</template>
+  
+<script>
+
+import { synRequestPost, synRequestGet } from "../../static/request"
+
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+    
+    <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
+    
