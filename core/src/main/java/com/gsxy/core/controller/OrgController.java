5711587af0c35d64ad7a664be71ce93503243d10
zhuxinyu <2909336700@qq.com>
2023-10-23 16:44:30 +0800

    包的补充

---
+++ core/src/main/java/com/gsxy/core/controller/OrgController.java
@@ -0,0 +1,18 @@
+package com.gsxy.core.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@Api(value = "班级版块接口", tags = {"班级板块接口"})
+@RestController
+@RequestMapping("/org")
+public class OrgController {
+
+
+
+}
