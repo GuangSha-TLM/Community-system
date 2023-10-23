a44ca3be608cd542719074adb3cbd73910c8fc89
Oh...Yeah!!! <614988210@qq.com>
2023-10-23 17:31:13 +0800

    Img的curd

---
+++ core/src/main/java/com/gsxy/core/controller/ImgController.java
@@ -0,0 +1,81 @@
+package com.gsxy.core.controller;
+
+import com.alibaba.fastjson2.JSONArray;
+import com.gsxy.core.domain.bo.ImgAddByBo;
+import com.gsxy.core.domain.bo.ImgDeleteByIdBo;
+import com.gsxy.core.domain.bo.ImgSelectByIdBo;
+import com.gsxy.core.domain.bo.ImgUpdateByIdBo;
+import com.gsxy.core.service.ImgService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ *  2023-10-12
+ *  图片板块接口
+ */
+@CrossOrigin
+@Api(value = "图片版块接口",tags = {"图片版块接口"})
+@RestController
+@RequestMapping("/img")
+public class ImgController {
+
+    @Autowired
+    private ImgService imgService;
+
+    /**
+     * @author Oh… Yeah!!!, 2023-10-23
+     *      根据id查询数据.
+     * @param imgSelectByIdBo
+     * @return String.class
+     */
+    @PostMapping("/select")
+    @ApiOperation("根据id查询img数据")
+    public String imgSelect(@RequestBody ImgSelectByIdBo imgSelectByIdBo){
+        return JSONArray.toJSONString(imgService.imgSelectById(imgSelectByIdBo));
+    }
+
+
+    /**
+     * @author Oh… Yeah!!!, 2023-10-23
+     *      通过id删除Img数据.
+     * @param imgDeleteByIdBo
+     * @return String.class
+     */
+    @PostMapping("/delete")
+    @ApiOperation("通过id删除img数据")
+    public String taskImgDeleteById(@RequestBody ImgDeleteByIdBo imgDeleteByIdBo){
+        return JSONArray.toJSONString(imgService.imgDeleteById(imgDeleteByIdBo));
+    }
+
+
+    /**
+     * @author Oh… Yeah!!!, 2023-10-23
+     *      增加新数据.
+     * @param imgAddByBo
+     * @return String.class
+     */
+    @PostMapping("/add")
+    @ApiOperation("增加img数据")
+    public String imgAdd(@RequestBody ImgAddByBo imgAddByBo){
+        return JSONArray.toJSONString(imgService.imgAdd(imgAddByBo));
+    }
+
+
+
+    /**
+     * @author Oh… Yeah!!!, 2023-10-23
+     *      通过id更新Img数据.
+     * @param imgUpdateByIdBo
+     * @return String.class
+     */
+    @ApiOperation("通过id修改Img数据")
+    @PostMapping("/update")
+    public String taskImgUpdateById(@RequestBody ImgUpdateByIdBo imgUpdateByIdBo){
+        return JSONArray.toJSONString(imgService.imgUpdateById(imgUpdateByIdBo));
+    }
+
+
+
+}
---
+++ core/src/main/java/com/gsxy/core/mapper/ImgMapper.java
@@ -0,0 +1,44 @@
+package com.gsxy.core.mapper;
+
+import com.gsxy.core.domain.Img;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ *  2023-10-23
+ *  图片持久层
+ */
+@Mapper
+public interface ImgMapper {
+
+    /**
+     * @author Oh… Yeah!!!, 2023-10-23
+     *      根据id查询数据.
+     * @param id
+     * @return Img.class
+     */
+    public Img selectByIdImg(Long id);
+
+    /**
+     * @author Oh… Yeah!!!, 2023-10-23
+     *      通过id删除Img数据.
+     * @param id
+     * @return String.class
+     */
+    public Long deleteByIdImg(Long id);
+
+    /**
+     * @author Oh… Yeah!!! 2023-10-23
+     *      增加新数据.
+     * @param img
+     * @return String.class
+     */
+    public Long addImg(Img img);
+
+    /**
+     * @author Oh… Yeah, 2023-10-23
+     *      通过id更新Img数据.
+     * @param img
+     * @return String.class
+     */
+    public Long updateByIdImg(Img img);
+}
