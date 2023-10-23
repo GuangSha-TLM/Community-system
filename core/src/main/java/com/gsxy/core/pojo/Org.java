5541b336651866f474dc786fb7b01f30e6582124
zhuxinyu <2909336700@qq.com>
2023-10-23 16:15:48 +0800

    班级实体类

---
+++ core/src/main/java/com/gsxy/core/pojo/Org.java
@@ -0,0 +1,106 @@
+package com.gsxy.core.pojo;
+
+import javax.xml.crypto.Data;
+
+public class Org {
+    private String name;
+    private String createBy;
+    private Data createTime;
+    private String updateBy;
+    private Data updateTime;
+    private Integer stauts;
+    private Integer del_flag;
+    private String remark;
+    public Org(){
+
+    }
+
+    public Org(String name, String createBy, Data createTime, String updateBy, Data updateTime, Integer stauts, Integer del_flag, String remark) {
+        this.name = name;
+        this.createBy = createBy;
+        this.createTime = createTime;
+        this.updateBy = updateBy;
+        this.updateTime = updateTime;
+        this.stauts = stauts;
+        this.del_flag = del_flag;
+        this.remark = remark;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public Data getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Data createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy;
+    }
+
+    public Data getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Data updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Integer getStauts() {
+        return stauts;
+    }
+
+    public void setStauts(Integer stauts) {
+        this.stauts = stauts;
+    }
+
+    public Integer getDel_flag() {
+        return del_flag;
+    }
+
+    public void setDel_flag(Integer del_flag) {
+        this.del_flag = del_flag;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    @Override
+    public String toString() {
+        return "Org{" +
+                "name='" + name + '\'' +
+                ", createBy='" + createBy + '\'' +
+                ", createTime=" + createTime +
+                ", updateBy='" + updateBy + '\'' +
+                ", updateTime=" + updateTime +
+                ", stauts=" + stauts +
+                ", del_flag=" + del_flag +
+                ", remark='" + remark + '\'' +
+                '}';
+    }
+}
