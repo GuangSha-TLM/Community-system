50ae404c991e82f2b9b64731e3f4775e7862f49e
Jasper Han <18945342298@163.com>
2023-10-25 12:05:23 +0800

    用户板块（根据id查询用户信息加入token） hln 2023-10-25 12:04

--- core/src/main/java/com/gsxy/core/service/impl/UserServiceImpl.java
+++ core/src/main/java/com/gsxy/core/service/impl/UserServiceImpl.java
@@ -80,9 +80,13 @@ public class UserServiceImpl implements UserService {
         String userIdOfStr = (String) ThreadLocalUtil.mapThreadLocalOfJWT.get().get("userinfo").get("id");
         Long userId = Long.valueOf(userIdOfStr);
 
-        List<User> list = userMapper.selectByUserId(userId);
+        if(userId == null || userId == 0L){
+            return new ResponseVo("token解析失败",null,"0x501");
+        }
 
-        if(list == null || userId == 0L){
+        List<User> list = userMapper.selectByUserId(userSelectByUserIdBo.getId());
+
+        if(list == null){
             return new ResponseVo("查询条件不存在",null,"0x500");
         }
 
