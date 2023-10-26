2c0eab61df228038dafeb9f538822e10ed52ce3e
chenzhixiang <y51288033@outlook.com>
2023-10-26 12:28:13 +0800

    用户模块对接和Home组件的搭建

---
+++ community-ui/src/components/Home.vue
@@ -0,0 +1,82 @@
+<template>
+    <!-- mainconten -->
+    <div class="hello">
+        <div class="top">
+            <Top></Top>
+        </div>
+        <div class="content">
+            <div class="left">
+                <div class="complaintManagement tab" v-if="Admin">
+                    <router-link to="/ClassManagement">班级管理</router-link>
+                </div>
+                <div class="userMangement tab" >
+                    <router-link to="/userManagement">用户管理</router-link>
+                </div>
+                <div class="userInfo tab">
+                    <router-link to="/AssociationManager">社团管理</router-link>
+                </div>
+            </div>
+            <div class="rigth">
+                <router-view />
+            </div>
+        </div>
+
+        <footer class="position-relative" id="footer-main">
+            <Foot></Foot>
+        </footer>
+    </div>
+</template>
+<script>
+import Foot from './fream/Foot.vue';
+import Top from './fream/LoginTop.vue'
+import { synRequestPost, synRequestGet } from "../../static/request"
+
+export default {
+    name: 'Register',
+    components: {
+        Foot, Top
+    },
+    data() {
+        return {
+            // Admin: false
+            Admin: true
+        }
+    },
+    methods: {
+        isAdmin() {
+            let role = JSON.parse(localStorage.getItem('role'))
+            if (role == 2) {
+                this.Admin = true
+            }
+        }
+    },
+    mounted() {
+        this.isAdmin()
+    }
+
+}
+</script>
+<style scoped>
+.content {
+    display: flex;
+
+}
+
+.left {
+    height: 90%;
+    width: 15%;
+}
+
+.rigth {
+    height: 90%;
+    width: 85%;
+
+}
+
+.tab {
+    margin: 30px;
+}
+
+a {
+    color: black;
+}</style>
