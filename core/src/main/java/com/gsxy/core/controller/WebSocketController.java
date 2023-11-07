8ed91546221f6b6dac8c31f724d637311d2f6aab
Jasper Han <18945342298@163.com>
2023-11-07 20:12:27 +0800

    websocket hln 2023-11-07 19:07

--- core/src/main/java/com/gsxy/core/controller/WebSocketController.java
+++ core/src/main/java/com/gsxy/core/controller/WebSocketController.java
@@ -1,77 +1,79 @@
-package com.gsxy.core.controller;
-
-import com.gsxy.core.mapper.UserAdminMapper;
-import com.gsxy.core.mapper.UserMapper;
-import com.gsxy.core.pojo.bo.SignInAdminWebSocketBo;
-import com.gsxy.core.pojo.bo.SignInWebSocketBo;
-import com.gsxy.core.service.UserAdminService;
-import com.gsxy.core.service.UserService;
-import com.gsxy.core.util.SpringContextUtil;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import javax.websocket.*;
-import javax.websocket.server.PathParam;
-import javax.websocket.server.ServerEndpoint;
-import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
-
-@ServerEndpoint("/websocket")
-@CrossOrigin
-@Component
-public class WebSocketController {
-
-    private  Set<Session> sessions = new HashSet<>();
-
-    private UserAdminService userAdminService = SpringContextUtil.getBean(UserAdminService.class);
-
-    private UserService userService = SpringContextUtil.getBean(UserService.class);
-
-    @OnOpen
-    public void onOpen(Session session, @PathParam("signinadminwebsocketbo")SignInAdminWebSocketBo signInAdminWebSocketBo){
-        userAdminService.adminSignInWeb(signInAdminWebSocketBo);
-        System.out.println("签到已发起");
-    }
-
-    @OnMessage
-    public void onMessage(String message, Session session, @PathParam("signinwebsocketbo")SignInWebSocketBo signInWebSocketBo) throws IOException {
-
-        //处理签到请求
-        if(message.startsWith("签到: ")){
-            String username = message.substring(5);
-
-            //记录签到信息，可以将签到信息存储在数据库中
-            userService.userSignInWebSocket(signInWebSocketBo);
-            System.out.println(username + "已签到!");
-            //发送签到响应
-            String response = "你已签到" + username;
-            session.getBasicRemote().sendText(response);
-
-            //向所有连接的客户端广播签到信息
-            broadcast(username + "已签到!");
-        }
-
-    }
-
-    @OnClose
-    public void onClose(Session session){
-        System.out.println("WebSocket连接已关闭");
-    }
-
-    @OnError
-    public void onError(Throwable error){
-        System.out.println("WebSocket错误: " + error.getMessage());
-    }
-
-    //回显用户签到信息到客户端
-    private void broadcast(String message){
-        for (Session session : sessions) {
-            try {
-                session.getBasicRemote().sendText(message);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-}
+//package com.gsxy.core.controller;
+//
+//import com.gsxy.core.mapper.UserAdminMapper;
+//import com.gsxy.core.mapper.UserMapper;
+//import com.gsxy.core.pojo.bo.SignInAdminWebSocketBo;
+//import com.gsxy.core.pojo.bo.SignInWebSocketBo;
+//import com.gsxy.core.service.UserAdminService;
+//import com.gsxy.core.service.UserService;
+//import com.gsxy.core.util.SpringContextUtil;
+//import org.springframework.scheduling.annotation.EnableScheduling;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import javax.websocket.*;
+//import javax.websocket.server.PathParam;
+//import javax.websocket.server.ServerEndpoint;
+//import java.io.IOException;
+//import java.util.HashSet;
+//import java.util.Set;
+//
+//@ServerEndpoint("/websocket")
+//@CrossOrigin
+//@Component
+//@EnableScheduling
+//public class WebSocketController {
+//
+//    private  Set<Session> sessions = new HashSet<>();
+//
+//    private UserAdminService userAdminService = SpringContextUtil.getBean(UserAdminService.class);
+//
+//    private UserService userService = SpringContextUtil.getBean(UserService.class);
+//
+//    @OnOpen
+//    public void onOpen(Session session, @PathParam("signinadminwebsocketbo")SignInAdminWebSocketBo signInAdminWebSocketBo){
+//        userAdminService.adminSignInWeb(signInAdminWebSocketBo);
+//        System.out.println("签到已发起");
+//    }
+//
+//    @OnMessage
+//    public void onMessage(String message, Session session, @PathParam("signinwebsocketbo")SignInWebSocketBo signInWebSocketBo) throws IOException {
+//
+//        //处理签到请求
+//        if(message.startsWith("签到: ")){
+//            String username = message.substring(5);
+//
+//            //记录签到信息，可以将签到信息存储在数据库中
+//            userService.userSignInWebSocket(signInWebSocketBo);
+//            System.out.println(username + "已签到!");
+//            //发送签到响应
+//            String response = "你已签到" + username;
+//            session.getBasicRemote().sendText(response);
+//
+//            //向所有连接的客户端广播签到信息
+//            broadcast(username + "已签到!");
+//        }
+//
+//    }
+//
+//    @OnClose
+//    public void onClose(Session session){
+//        System.out.println("WebSocket连接已关闭");
+//    }
+//
+//    @OnError
+//    public void onError(Throwable error){
+//        System.out.println("WebSocket错误: " + error.getMessage());
+//    }
+//
+//    //回显用户签到信息到客户端
+//    private void broadcast(String message){
+//        for (Session session : sessions) {
+//            try {
+//                session.getBasicRemote().sendText(message);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+//
+//}
