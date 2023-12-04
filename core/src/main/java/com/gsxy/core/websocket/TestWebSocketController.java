4a06e99e85645df9c8c7f7fcb156d26f58464d0e
tianleiyu <2540675003@qq.com>
2023-12-04 14:27:21 +0800

    解决websocket传输问题

--- core/src/main/java/com/gsxy/core/websocket/TestWebSocketController.java
+++ core/src/main/java/com/gsxy/core/websocket/TestWebSocketController.java
@@ -8,12 +8,15 @@ import com.gsxy.core.util.ThreadLocalUtil;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 @ServerEndpoint("/websocket/{id}")
 @CrossOrigin
@@ -23,10 +26,12 @@ public class TestWebSocketController {
     // 构造函数注入任何需要的依赖项
     private UserAdminController userAdminController = SpringContextUtil.getBean(UserAdminController.class);
 //    private UserAdminMapper userAdminMapper = SpringContextUtil.getBean(UserAdminMapper.class);
-
+//private static final Set<Session> sessions = new CopyOnWriteArraySet<>();
     @OnOpen
     public void onOpen(Session session, @PathParam("id") Long id) throws IOException {
+//        sessions.add(session);
         String s = this.serviceFunction(id, session);
+//        broadcastMessage(s);
         session.getBasicRemote().sendText(s);
     }
 
@@ -34,9 +39,21 @@ public class TestWebSocketController {
     @OnMessage
     public void onMessage(Long id, Session session) throws IOException {
 
+//        System.out.println(id);
         //实现用户签到信息的实时查看
         String s = this.serviceFunction(id, session);
+//        broadcastMessage(s);
         session.getBasicRemote().sendText(s);
+
+    }
+
+    @OnClose
+    public void onClose(Session session) {
+//        sessions.remove(session);
+//        System.out.println(session);
+//        System.out.println("连接关闭");
+//        System.out.println("WebSocket closed for session: " + session.getId());
+
 
     }
 
@@ -47,6 +64,19 @@ public class TestWebSocketController {
 
         return str;
     }
+//    private static void broadcastMessage(String message) {
+//        for (Session session : sessions) {
+//            if (session.isOpen()) {
+//                try {
+//                    session.getBasicRemote().sendText(message);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//    }
+
+
 
 
 }
